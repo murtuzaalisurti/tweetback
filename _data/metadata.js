7790d455fc9aa5e7072a4d6039d8915a353f57f0
murtuza <murtuza.creativity@gmail.com>
2022-12-03 19:02:51 +0530

    populated data

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,7 +1,7 @@
 let data = {
-	username: "eleven_ty", // No leading @ here
-	homeLabel: "11ty.dev",
-	homeUrl: "https://www.11ty.dev/",
+	username: "murtuza_surti", // No leading @ here
+	homeLabel: "Murtuzaali Surti",
+	homeUrl: "https://murtuzaalisurti.github.io",
 };
 
 data.avatar = `https://v1.indieweb-avatar.11ty.dev/${encodeURIComponent(data.homeUrl)}/`;
